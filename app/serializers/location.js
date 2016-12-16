b58392ad645e14d842dadde8670bab883068aaaf
tsetsova <inatsetsova@gmail.com>
2016-12-16 13:03:02 +0000

    [Ina/Caroline] the serializer is no longer hard coded to location

--- app/serializers/location.js
+++ app/serializers/location.js
@@ -1,8 +1,8 @@
 import JSONAPISerializer from "ember-data/serializers/json-api";
 
 export default JSONAPISerializer.extend({
-    normalizeResponse(store, primaryModelClass, payload, id, requestType) {
-        payload = {data: {type: 'location',
+    normalizeFindRecordResponse(store, primaryModelClass, payload, id, requestType) {
+        payload = {data: {type: primaryModelClass.modelName,
                           id: payload.items[0].fields.slug,
                           attributes: payload.items[0].fields}};
         return this._super(store, primaryModelClass, payload, id, requestType);
