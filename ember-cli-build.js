399a1e82139f86060b6f6071bf342bb0cdab4c6b
Caroline Kok <ckok@thoughtworks.com>
2016-12-21 13:41:07 +0000

    Mark/Caroline: using foundation 5 as grid system.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,10 +4,12 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
+      sassOptions: {
+          includePaths: [
+              'bower_components/foundation/scss'
+          ]
+      }
   });
-
-  app.import('bower_components/normalize-css/normalize.css');
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
