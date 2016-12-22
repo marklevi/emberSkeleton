c1e7fc4343e0b3372a8d2615bb7352f84c74cbe4
Mark Levi <mlevi@thoughtworks.com>
2016-12-22 17:45:16 +0000

    slight enchancement to handlebar helper test

--- tests/unit/helpers/link-test.js
+++ tests/unit/helpers/link-test.js
@@ -1,12 +1,11 @@
-
-import { link } from 'cdg-frontend/helpers/link';
-import { module, test } from 'qunit';
+import Handlebars from "npm:handlebars";
+import {link} from "cdg-frontend/helpers/link";
+import {module, test} from "qunit";
 
 module('Unit | Helper | link');
 
-// Replace this with your real tests.
-test('it works', function(assert) {
-  let result = link([42]);
-  assert.ok(result);
+test('should resturn an instance of handlebars safeString', function(assert) {
+  let result = link([42,-1]);
+  assert.ok(result instanceof Handlebars.SafeString);
 });
 
