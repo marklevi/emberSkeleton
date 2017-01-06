d6c8cdbefd94391b50e3b8a22fa1c3b3a555c21c
Mark Levi <mlevi@thoughtworks.com>
2017-01-06 16:42:42 +0000

    Ina/Mark #33 display menu item variation and its corresponding price

--- tests/integration/components/restaurant-menu-test.js
+++ tests/integration/components/restaurant-menu-test.js
@@ -11,3 +11,12 @@ test('it renders the restaurant menu', function(assert) {
 
     assert.equal(this.$('.location__menu-title').text(), 'nice!');
 });
+
+test('it renders menu item variations', function(assert) {
+    this.set('model', {priceBand: "L", menu: {displayTitle: "Dinner", submenus: [{displayTitle: "From the flames", menuItems:[{displayTitle: "Enchilada", menuItemVariation: [{displayTitle: "Roast butternut squash, red peppers, spinach and cheese", pricebandL: "12.95"}]}]}]}});
+    this.render(hbs`{{restaurant-menu menu-with-price-band=model}}`);
+    assert.equal(this.$('.location__menu-title').text(), 'Dinner');
+    assert.equal(this.$('.menu-item-variation').first().text(), 'Roast butternut squash, red peppers, spinach and cheese');
+    assert.equal(this.$('.menu-item-variation-price').first().text(), '12.95');
+
+});
